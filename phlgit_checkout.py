2a817b72f526a5fcaecbc88247b5eb5a701b48ea
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-03-10 13:13:20 +0000

    phlgit_checkout: wrapper around 'git checkout'
    Test Plan: successfully used in yet-to-publish code

---
+++ phlgit_checkout.py
@@ -0,0 +1,35 @@
+"""Wrapper around 'git checkout'"""
+
+
+def newBranchForceBasedOn(clone, new_branch, base):
+    """Checkout onto a new branch copy of base, overwite existing branch.
+
+    :clone: the clone to operate on
+    :new_branch: the name for the new branch
+    :base: the name of the branch to copy
+    :returns: None
+
+    """
+    clone.call('checkout', '-B', new_branch, base)
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
