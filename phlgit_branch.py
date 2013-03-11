12713d0f6b39762f5fa415d8b8880d8003b67abe
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-03-11 11:50:23 +0000

    add phlgit_branch: wrapper around 'git branch'
    Test Plan: used in to-be-committed client code

---
+++ phlgit_branch.py
@@ -0,0 +1,52 @@
+"""Wrapper around 'git branch'"""
+
+
+def _getRefs(clone):
+    # the output list is like:
+    #     SHA1      Refname
+    #     SHA1      Refname
+    #     SHA1      Refname
+    out = clone.call('show-ref')
+    flat = out.split()  # convert to ['SHA1', 'Refname', 'SHA1', 'Refname']
+    # convert to [Refname, Refname, Refname]
+    refs = [flat[i + 1] for i in range(0, len(flat) - 1, 2)]
+    return refs
+
+
+def _filterRefsInNamespace(refs, namespace):
+    return [ref[len(namespace):] for ref in refs if ref.startswith(namespace)]
+
+
+def getLocal(clone):
+    refs = _getRefs(clone)
+    return _filterRefsInNamespace(refs, "refs/heads/")
+
+
+def getRemote(clone, remote):
+    refs = _getRefs(clone)
+    remote_head = "refs/remotes/" + remote + "/head"
+    refs = [r for r in refs if r != remote_head]
+    return _filterRefsInNamespace(refs, "refs/remotes/" + remote + "/")
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
