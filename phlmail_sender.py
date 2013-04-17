4eff7ce0f21f2a7b0e50efe01a2f2d50440419b0
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-04-17 18:02:09 +0100

    use new phlmail_sender to simplify slightly
    Test Plan: run maily and single instance tests in ./arcyd_test

---
+++ phlmail_sender.py
@@ -0,0 +1,43 @@
+import phlmail_format
+
+
+class MailSender(object):
+    """A mail sender that sends mail via a configured sendmail."""
+
+    def __init__(self, sendmail, from_email):
+        """Setup to print email to the console from 'from_email'.
+
+        :sendmail: the sendmail instance to send with, e.g. a phlsys_sendmail
+        :from_email: the address to send from
+
+        """
+        self._sendmail = sendmail
+        self._from_email = from_email
+
+    def send(self, to_addresses, subject, message, cc_addresses=None):
+        mime = phlmail_format.Text(
+            subject, message, self._from_email, to_addresses, cc_addresses)
+        self._sendmail.send(mime)
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
