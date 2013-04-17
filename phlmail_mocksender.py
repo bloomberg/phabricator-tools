04e8bcf08af3ba835481eff646fdc3c1448b3c8f
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-04-17 18:42:52 +0100

    add phlmail_mocksender to make testing easier
    Test Plan:
    add breathing tests, $ nosetests phlmail_mocksender__t

---
+++ phlmail_mocksender.py
@@ -0,0 +1,57 @@
+import collections
+import types
+
+Mail = collections.namedtuple(
+    'phlmail_mocksender__Mail',
+    ['subject', 'message', 'to_addresses', 'cc_addresses'])
+
+
+class MailSender(object):
+    """A mail sender that just stores mail to make testing easier."""
+
+    def __init__(self):
+        """Setup to store sent email."""
+        self._mailboxes = collections.defaultdict(list)
+        self._mails = []
+
+    def send(self, subject, message, to_addresses, cc_addresses=None):
+        assert not isinstance(to_addresses, types.StringTypes)
+        assert not isinstance(cc_addresses, types.StringTypes)
+        mail = Mail(subject, message, to_addresses, cc_addresses)
+        for address in to_addresses:
+            self._mailboxes[address].append(mail)
+        if cc_addresses is not None:
+            for address in cc_addresses:
+                self._mailboxes[address].append(mail)
+        self._mails.append(mail)
+
+    @property
+    def mailboxes(self):
+        return self._mailboxes
+
+    @property
+    def mails(self):
+        return self._mails
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
---
+++ phlmail_mocksender__t.py
@@ -0,0 +1,48 @@
+import unittest
+
+import phlmail_mocksender
+
+
+class Test(unittest.TestCase):
+    def test_empty(self):
+        mailsender = phlmail_mocksender.MailSender()
+        self.assertEqual(len(mailsender.mailboxes), 0)
+        self.assertEqual(len(mailsender.mails), 0)
+
+    def test_oneTo(self):
+        mailsender = phlmail_mocksender.MailSender()
+        subject = "subject"
+        message = "message"
+        to = "someone@server.test"
+        mailsender.send(subject, message, [to])
+        self.assertEqual(len(mailsender.mailboxes), 1)
+        self.assertEqual(len(mailsender.mails), 1)
+        self.assertEqual(mailsender.mailboxes[to][0], mailsender.mails[0])
+        mail = mailsender.mails[0]
+        self.assertEqual(mail.subject, subject)
+        self.assertEqual(mail.message, message)
+        self.assertEqual(mail.to_addresses, [to])
+        self.assertIsNone(mail.cc_addresses)
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
