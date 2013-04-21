317d3f59196d5590b2c4bf3e86e88388badb16be
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-04-21 19:31:18 +0100

    sweep vagrant helper scripts into vagrant dir

--- vagrant/vagrant-up-proxy.sh
+++ vagrant/vagrant-up-proxy.sh
@@ -1,5 +1,3 @@
-cd vagrant && vagrant up
-
 # note that if you have problems downloading the base box then you may
 # need to supply a proxy server, e.g. inside Bloomberg you want:
-#   cd vagrant && HTTP_PROXY="https://proxy.bloomberg.com:80" vagrant up
+HTTP_PROXY="https://proxy.bloomberg.com:80" vagrant up
--- vagrant/vagrant-windows-msysgit-ssh.sh
+++ vagrant/vagrant-windows-msysgit-ssh.sh
@@ -1,4 +1,3 @@
-ssh vagrant@127.0.0.1 -p 2222 -i $USERPROFILE/.vagrant.d/insecure_private_key
-
 # Vagrant on Windows won't happily ssh in, if you have MSysGit installed then
 # you can use this script (an ssh binary seems to come with MSysGit)
+ssh vagrant@127.0.0.1 -p 2222 -i $USERPROFILE/.vagrant.d/insecure_private_key
