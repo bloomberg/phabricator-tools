7592f51c8f87f6d3b2e54d1ed3bb1951b5c20440
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-05-04 11:03:29 +0100

    move subcommand wrapper into component
    Test Plan:
    testbed/ $ ./run-arcyd-single.sh
    $ arcyon show-config

---
+++ py/phlsys_subcommand.py
@@ -0,0 +1,41 @@
+"""Help to wrap up modules as subcommands."""
+
+import argparse
+
+
+def setupParser(name, module, subparsers):
+    doc = module.__doc__
+    docSubject = doc.splitlines()[0]
+    docEpilog = '\n'.join(doc.splitlines()[1:])
+    parser = subparsers.add_parser(
+        name,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        help=docSubject,
+        description=docSubject,
+        epilog=docEpilog,
+        fromfile_prefix_chars=module.getFromfilePrefixChars())
+    module.setupParser(parser)
+    parser.set_defaults(func=module.process)
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
