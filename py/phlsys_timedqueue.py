67da51f2ef317f819211674323269b9a74b61b0e
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-05-14 13:47:34 +0100

    phlsys_timedqueue: priority queue for delayed iems
    We need this for adding tasks to retry at some point in the future,
    e.g. when repositories have problems and we want to try them again in
    5 minutes or so.
    
    Test Plan: added phlsys_timedqueue__t

---
+++ py/phlsys_timedqueue.py
@@ -0,0 +1,55 @@
+"""Priority queue for objects with associated delays.
+
+Usage example:
+    >>> tq = TimedQueue(); tq.push('a', datetime.timedelta()); tq.pop_expired()
+    ['a']
+"""
+
+import datetime
+import heapq
+
+
+class TimedQueue(object):
+    def __init__(self):
+        self._items = []
+        self._counter = 0
+
+    def push(self, item, delay):
+        # here we use counter as a second item in the tuple to compare to make
+        # sure that heappush() never tries to compare two items, also to be
+        # sure that things come out in the same order they are pushed
+        heap_item = (datetime.datetime.now() + delay, self._counter, item)
+        self._counter += 1
+
+        heapq.heappush(self._items, heap_item)
+
+    def pop_expired(self):
+        expired = []
+        if self._items:
+            now = datetime.datetime.now()
+            while self._items and self._items[0][0] <= now:
+                expired.append(heapq.heappop(self._items)[2])
+        return expired
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
---
+++ py/phlsys_timedqueue__t.py
@@ -0,0 +1,108 @@
+"""Test suite for phlsys_timedqueue"""
+
+import unittest
+import datetime
+import sys
+
+import phlsys_timedqueue
+
+#==============================================================================
+#                                   TEST PLAN
+#------------------------------------------------------------------------------
+# Here we detail the things we are concerned to test and specify which tests
+# cover those concerns.
+#
+# Concerns:
+# [ B] a default-contructed TimedQueue is empty
+# [ B] an empty TimedQueue will pop_expired() []
+# [  ] a TimedQueue will never try to compare the items being stored
+# [ C] items are popped in the order they expire
+# [ C] items expire at or after their delay
+# [ C] items are only popped once
+# [ E] items are not referred to after they are popped
+#------------------------------------------------------------------------------
+# Tests:
+# [ A] test_A_Breathing
+# [ B] test_B_DefaultConstructor
+# [ C] test_C_PopOrderForward
+# [ D] test_D_PopOrderBackward
+# [ E] test_E_RefCountIncreaseDecrease
+#==============================================================================
+
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_A_Breathing(self):
+        pass
+
+    def test_B_DefaultConstructor(self):
+        tq = phlsys_timedqueue.TimedQueue()
+        self.assertListEqual([], tq.pop_expired())
+
+    def test_C_PopOrderForward(self):
+        tq = phlsys_timedqueue.TimedQueue()
+        a = "a"
+        b = "b"
+        c = "c"
+        no_time = datetime.timedelta()
+        tq.push(a, no_time)
+        tq.push(b, no_time)
+        tq.push(c, no_time)
+        self.assertListEqual([a, b, c], tq.pop_expired())
+        self.assertListEqual([], tq.pop_expired())
+
+    def test_D_PopOrderBackward(self):
+        tq = phlsys_timedqueue.TimedQueue()
+        a = "a"
+        b = "b"
+        c = "c"
+        tq.push(a, datetime.timedelta(-1))
+        tq.push(b, datetime.timedelta(-2))
+        tq.push(c, datetime.timedelta(-3))
+        self.assertListEqual([c, b, a], tq.pop_expired())
+        self.assertListEqual([], tq.pop_expired())
+
+    def test_E_RefCountIncreaseDecrease(self):
+        tq = phlsys_timedqueue.TimedQueue()
+        a = "a"
+        before_count = sys.getrefcount(a)
+        tq.push(a, datetime.timedelta())
+        during_count = sys.getrefcount(a)
+        tq.pop_expired()
+        after_count = sys.getrefcount(a)
+
+        # we expect before and after to be the same, assuming nothing external
+        # is aquiring references. (e.g. the Python debugger perhaps)
+        self.assertEqual(before_count, after_count)
+
+        # we expect the count to go up whilst the TimedQueue is holding our
+        # object, otherwise something must be wrong
+        self.assertGreater(during_count, before_count)
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
