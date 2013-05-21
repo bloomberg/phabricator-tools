f68795a64c3f7caf43f9b8c1edec15a7f64d98a3
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-05-21 17:29:51 +0100

    arcyon get-diff: simple command to retrieve diffs
    Initially just make it easy to get the list of affected files,
    can later reformat the corpus as a unified diff.
    
    Test Plan:
    play in testbed/
    testbed/ $ ./exercise_arcyon.sh

---
+++ py/aoncmd_getdiff.py
@@ -0,0 +1,159 @@
+"""Get a diff from differential, along with metadata.
+
+usage examples:
+    list the files affected by revision 1
+    $ arcyon get-diff -r 1 --ls
+    /file1
+    /dir/file2
+
+    list the branch and files
+    $ arcyon get-diff -r 1 --fs "{branch}" "{oldPath} -> {currentPath}"
+
+output formats:
+    --format-python
+        {u'bookmark': None,
+        u'branch': None,
+        u'changes': [{u'addLines': u'0',
+                    u'awayPaths': [],
+                    u'commitHash': None,
+                    u'currentPath': u'NEWFILE',
+                    u'delLines': u'0',
+                    ...
+
+    --format-json
+        {
+        "bookmark": null,
+        "branch": null,
+        "changes": [
+            {
+            "addLines": "0",
+            "awayPaths": [],
+            "commitHash": null,
+            "currentPath": "NEWFILE",
+            "delLines": "0",
+            ...
+"""
+
+import json
+import pprint
+
+import phlsys_makeconduit
+
+import aont_conduitargs
+
+
+def getFromfilePrefixChars():
+    return ""
+
+
+def setupParser(parser):
+    diffsrc_group = parser.add_argument_group(
+        'Diff arguments',
+        'Mutually exclusive, one is required')
+    diffsrc = diffsrc_group.add_mutually_exclusive_group(required=True)
+    output_group = parser.add_argument_group(
+        'Output format arguments',
+        'Mutually exclusive, defaults to "--list-files"')
+    output = output_group.add_mutually_exclusive_group()
+
+    diffsrc.add_argument(
+        '--revision',
+        '-r',
+        metavar='INT',
+        help='the id of the revision to get the diff from.',
+        type=int)
+    diffsrc.add_argument(
+        '--diff',
+        '-d',
+        metavar='INT',
+        help='the id of the diff to retrieve.',
+        type=int)
+
+    output.add_argument(
+        '--list-files',
+        '--ls',
+        action='store_true',
+        help='list only the affected paths, shortcut for the --format-type. '
+             'this lists the set of files found in the "currentPath" and '
+             '"oldPath" fields of the "changes" field.')
+    output.add_argument(
+        '--format-python',
+        action='store_true',
+        help='print python representation of the raw response from '
+             'the server.')
+    output.add_argument(
+        '--format-json',
+        action='store_true',
+        help='print json representation of the raw response from '
+             'the server.')
+    output.add_argument(
+        '--format-strings',
+        '--fs',
+        type=str,
+        metavar='STR',
+        nargs=2,
+        help='specify two custom format strings for displaying the items, '
+             'the first string is applied to the whole output, the second '
+             'is applied per item in the "changes" dictionary. i.e. '
+             '("FORMAT-DIFF", "FORMAT-FOREACH-CHANGE").  the strings will be '
+             'applied using Python\'s str.format(), so you can use '
+             'curly brackets to substitute for field names, e.g. "\{id\}". '
+             'you can use "--format-python" to discover the field names.')
+
+    aont_conduitargs.addArguments(parser)
+
+
+def process(args):
+    conduit = phlsys_makeconduit.makeConduit(args.uri, args.user, args.cert)
+
+    d = {}
+
+    if args.revision:
+        d["revision_id"] = args.revision
+    if args.diff:
+        d["diff_id"] = args.diff
+
+    result = conduit.call("differential.getdiff", d)
+
+    if args.format_python:
+        pprint.pprint(result)
+    if args.format_json:
+        print json.dumps(result, sort_keys=True, indent=2)
+    if args.format_strings:
+        fmt = args.format_strings[0]
+        fmt_change = args.format_strings[1]
+        if fmt:
+            print fmt.format(**result)
+        if fmt_change:
+            for change in result["changes"]:
+                print fmt_change.format(**change)
+    else:  # args.list_files:
+        paths = set()
+        for change in result["changes"]:
+            paths.add(change["currentPath"])
+            paths.add(change["oldPath"])
+        for path in paths:
+            print path
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
--- testbed/exercise_arcyon.sh
+++ testbed/exercise_arcyon.sh
@@ -4,12 +4,16 @@ set -x
 arcyon='../bin/arcyon'
 
 id="$($arcyon create-revision -t title -p plan -f diff1)"
+$arcyon get-diff -r $id --ls
 $arcyon update-revision $id update -f diff2
+$arcyon get-diff -r $id --ls
 
 $arcyon query --format-type ids | grep $id
 
 diffid="$($arcyon raw-diff diff1)"
 diffid2="$($arcyon raw-diff diff2)"
+$arcyon get-diff -d $diffid --ls
+$arcyon get-diff -d $diffid2 --ls
 id2="$($arcyon create-revision -t title2 -p plan --diff-id $diffid)"
 $arcyon update-revision $id2 update --diff-id $diffid2
 
