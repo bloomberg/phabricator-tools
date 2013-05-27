ae7c579a4c6db8422edd54edf2333608371dc918
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-05-27 23:52:35 +0100

    testbed/phab-ping: add 'watch, mail on fail"
    Add a simple utility to watch a Phabricator instance and mail if
    it become unavailable.

---
+++ testbed/phab-ping/tool_watch_instance_mail_on_fail.sh
@@ -0,0 +1,33 @@
+set -e
+address=$1
+from_email=$2
+to_email=$3
+
+if [ ! "$#" = "3" ]; then
+    echo "usage: $0 phabricator-address from-email to-email"
+    exit 2
+fi
+
+# uncomment to use catchmail
+#sendmail="--sendmail-binary catchmail --sendmail-type catchmail"
+sendmail="--sendmail-binary sendmail --sendmail-type sendmail"
+
+maily="../arcyd/maily --sender $from_email --to $to_email $sendmail"
+$maily --subject "started pinging $address" --message " "
+tempfile=`mktemp`
+echo "pinging $address"
+set +e
+
+../../bin/phab-ping $address > $tempfile
+result=`(head -n 20; echo === snip ===;tail -n 20) < $tempfile`
+rm $tempfile
+if [ "$?" = "1" ]; then
+    $maily --subject "failed pinging $address" --message "$result"
+    echo "$result"
+    echo FAILED
+    exit 1
+else
+    $maily --subject "stopped pinging $address" --message "$result"
+    echo "$result"
+    echo STOPPED
+fi
