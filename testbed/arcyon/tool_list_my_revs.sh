36cf09011ee02c3d880d8d8a5107db4886bc1ff7
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-05-28 00:11:50 +0100

    testbed/arcyon: add tool_list_my_revs.sh
    List revisions that you are involved in

---
+++ testbed/arcyon/tool_list_my_revs.sh
@@ -0,0 +1,5 @@
+echo Open reviews where you are the author:
+../../bin/arcyon query --author-me --status-type open
+
+echo Open reviews where you are a reviewer:
+../../bin/arcyon query --reviewer-me --status-type open
