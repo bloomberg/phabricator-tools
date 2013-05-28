0de7e10acaa580916728190eec44676ef2eea844
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-05-28 10:25:42 +0100

    testbed/arcyon: add tool_nudge_stale_waiting_on.sh
    Add a simple tool which discovers stale reviews and comments on
    them with 'nudge'.

---
+++ testbed/arcyon/tool_nudge_stale_waiting_on.sh
@@ -0,0 +1,18 @@
+# comment 'nudge' on reviews which we've been waiting on for over 2 days
+
+arcyon="../../bin/arcyon"
+minage="2 days"
+ids1=`$arcyon query --author-me --status-type open --statuses 'Needs Review' --format-type ids --update-min-age "$minage"`
+ids2=`$arcyon query --reviewer-me --status-type open --statuses 'Needs Revision' --format-type ids --update-min-age "$minage"`
+
+echo "will comment 'nudge' on the following reviews:"
+ids=`echo $ids1 $ids2`
+echo $ids
+
+read -p "Hit 'y' to continue or any other to exit: " choice
+if [ ! "$choice" = "y" ]; then
+    echo user aborted.
+    exit 2
+fi
+
+$arcyon comment $ids -m 'nudge'
