7b69d065765474ca6c1d0cc482ec01ba8ce01d95
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-05-31 22:34:15 +0100

    testbed/arcyon/tool_list_waiting_on: fix comment

--- testbed/arcyon/tool_list_waiting_on.sh
+++ testbed/arcyon/tool_list_waiting_on.sh
@@ -1,4 +1,4 @@
-# List out the revisions that require the user to act next.
+# List out the revisions that require another user to act next.
 
 echo Revisions you have authored, waiting on others:
 ../../bin/arcyon query --author-me --status-type open --statuses 'Needs Review'
