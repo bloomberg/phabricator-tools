937ae06685295287277eab281d2a32fff3a58f9b
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-06-13 21:35:28 +0100

    add phlsys_namedtuple: helper for unstable schemas
    Add a wrapper around collections.namedtuple which will allow us to
    easily marshall dictionaries into namedtuples with the flexibility to
    specify required, default and ignored parameters.
    
    This is necessary to provide some defined flexibility in the schemas of
    the data that we receive from Phabricator.
    
    Test Plan: $ ./run_all_tests.sh

---
+++ py/phl/phlsys_namedtuple.py
@@ -0,0 +1,118 @@
+"""Wrapper around collections.namedtuple with some added features.
+
+The 'collections.namedtuple' is very useful as a data transfer object,
+when communicating with Phabricator it's important to consider that the
+schema for the objects can change on the server side before we get a chance
+to update our client.
+
+This namedtuple aims to build a layer of tolerance on top of namedtuple such
+that the client can continue to function when the schema changes within defined
+parameters.
+
+"""
+
+import collections
+
+
+class Error(Exception):
+    pass
+
+
+def make_named_tuple(name, required, defaults, ignored):
+    """Return a factory function for collection.namedtuple objects which uses
+    the supplied 'required', 'defaults' and 'ignored' parameters to determine
+    what the attributes of the namedtuple should be.
+
+    All items from the supplied 'required' list must be provided in each
+    call to the factory function.
+
+    Keys from the specified 'defaults' dict may or may not be provided
+    in calls to the returned factory function, for those that are not
+    provided then the values are taken from the corresponding keys in the
+    'defaults' dict.
+
+    Items from the specified 'ignored' list are removed from the keyword
+    arguments provided to the factory function prior to constructing the
+    namedtuple.
+
+    Usage Examples:
+
+        Create a factory which requires 'number' and returns 'MyTuple's:
+        >>> factory = make_named_tuple("MyTuple", ['number'], {}, [])
+        >>> factory(number=1)
+        MyTuple(number=1)
+
+        Create a factory which ignores 'number' returns 'MyTuple's:
+        >>> factory = make_named_tuple("MyTuple", [], {}, ['number'])
+        >>> factory(number=1)
+        MyTuple()
+
+        Create a factory which defaults 'number' to 2 and returns 'MyTuple's:
+        >>> factory = make_named_tuple("MyTuple", [], {'number': 2}, [])
+        >>> factory()
+        MyTuple(number=2)
+
+    :name: string name of the collections.namedtuple
+    :required: list of keys required to be supplied to the factory function
+    :defaults: dict of default values to be filled in by the factory function
+    :ignored: list of keys for the factory function to ignore
+    :returns: factory function which returns a collections.namedtuple
+
+    """
+    required_attr = set(required)
+    default_attr = dict(defaults)
+    default_attr_keys = default_attr.viewkeys()
+    ignored_attr = set(ignored)
+    assert not (default_attr_keys & required_attr)
+    assert not (default_attr_keys & ignored_attr)
+    assert not (ignored_attr & required_attr)
+    NamedTuple = collections.namedtuple(name, required + defaults.keys())
+
+    # define the factory function
+    def make_instance(**kwargs):
+        keys = kwargs.viewkeys()
+
+        # remove all ignored_attr from kwargs
+        ignored_keys = keys & ignored_attr
+        for key in ignored_keys:
+            del kwargs[key]
+
+        # warn programmers if we need to handle more attributes, users should
+        # be running optimised, so assertions won't be firing
+        unexpected = keys - (required_attr | default_attr_keys)
+        assert not unexpected, "unexpected args: " + str(unexpected)
+
+        missing_attr = required_attr - keys
+        if missing_attr:
+            raise Error("missing attributes: " + str(missing_attr))
+
+        auto_attr = default_attr_keys - keys
+        for a in auto_attr:
+            kwargs[a] = default_attr[a]
+
+        return NamedTuple(**kwargs)
+
+    return make_instance
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
---
+++ py/phl/phlsys_namedtuple__t.py
@@ -0,0 +1,141 @@
+"""Test suite for phlsys_namedtuple"""
+
+import unittest
+
+import phlsys_namedtuple
+
+#==============================================================================
+#                                   TEST PLAN
+#------------------------------------------------------------------------------
+# Here we detail the things we are concerned to test and specify which tests
+# cover those concerns.
+#
+# In the interests of brevity, we'll refer to
+# 'phlsys_namedtuple.make_named_tuple()' as 'make()' from here on in.
+#
+# In the interests of further brevity, we'll refer to functions returned by
+# make() collectively as factory() from here on in.
+#
+# Concerns:
+# [ A] factory() returns a seemingly regular namedtuple
+# [ B] factory() raises a phlsys_namedtuple.Error if 'required' contains
+#      items which weren't supplied to factory().
+# [ D] factory() ignores parameters mentioned in 'ignored' without event
+# [ C] factory() asserts if parameters not mentioned in 'required', 'defaults'
+#      and 'ignored' are encountered.
+# [ E] make() asserts if 'required', 'default' and 'ignored' are not disjoint
+# [ F] subsequent calls to make() don't affect previously created factories
+#------------------------------------------------------------------------------
+# Tests:
+# [ A] test_A_Breathing
+# [ B] test_B_RaiseMissing
+# [ C] test_C_AssertUnexpected
+# [ D] test_D_ignoreIgnored
+#==============================================================================
+
+
+class Test(unittest.TestCase):
+    def __init__(self, data):
+        self.factory = None
+        super(Test, self).__init__(data)
+
+    def setUp(self):
+        self.factory = phlsys_namedtuple.make_named_tuple(
+            'T',
+            required=['r1', 'r2'],
+            defaults={'d1': 1, 'd2': 2},
+            ignored=['i1', 'i2'])
+
+    def tearDown(self):
+        pass
+
+    def test_A_Breathing(self):
+        t = self.factory(r1=1, r2=2)
+        self.assertEqual(t.r1, 1)
+        self.assertEqual(t.r2, 2)
+        self.assertEqual(t.d1, 1)
+        self.assertEqual(t.d2, 2)
+
+    def test_B_RaiseMissing(self):
+        with self.assertRaises(phlsys_namedtuple.Error):
+            self.factory(r1=1)
+        with self.assertRaises(phlsys_namedtuple.Error):
+            self.factory(r2=2)
+        with self.assertRaises(phlsys_namedtuple.Error):
+            self.factory()
+        with self.assertRaises(phlsys_namedtuple.Error):
+            self.factory(d1=1)
+        with self.assertRaises(phlsys_namedtuple.Error):
+            self.factory(d2=2)
+
+    def test_C_AssertUnexpected(self):
+        with self.assertRaises(AssertionError):
+            self.factory(r1=1, r2=2, dingbat=3)
+        with self.assertRaises(AssertionError):
+            self.factory(r1=1, r2=2, dingbat=3, d1=2, d2=3)
+        with self.assertRaises(AssertionError):
+            self.factory(r1=1, r2=2, dingbat=3, d1=2, d2=3, i1=-1, i2=-2)
+
+    def test_D_IgnoreIgnored(self):
+        self.factory(r1=1, r2=2, i1=-1)
+        self.factory(r1=1, r2=2, i2=1)
+        self.factory(r1=1, r2=2, i1=1, i2=1)
+
+    def test_E_AssertDisjoint(self):
+        with self.assertRaises(AssertionError):
+            phlsys_namedtuple.make_named_tuple(
+                'T',
+                required=['r1', 'r2'],
+                defaults={'r1': 1, 'd2': 2},
+                ignored=['i1', 'i2'])
+        with self.assertRaises(AssertionError):
+            phlsys_namedtuple.make_named_tuple(
+                'T',
+                required=['r1', 'r2'],
+                defaults={'d1': 1, 'd2': 2},
+                ignored=['r1', 'i2'])
+        with self.assertRaises(AssertionError):
+            phlsys_namedtuple.make_named_tuple(
+                'T',
+                required=['r1', 'r2'],
+                defaults={'d1': 1, 'd2': 2},
+                ignored=['d1', 'i2'])
+        with self.assertRaises(AssertionError):
+            phlsys_namedtuple.make_named_tuple(
+                'T',
+                required=['a', 'r2'],
+                defaults={'a': 1, 'd2': 2},
+                ignored=['a', 'i2'])
+
+    def test_F_IndependentFactory(self):
+        factory1 = phlsys_namedtuple.make_named_tuple('T1', [], {}, [])
+        phlsys_namedtuple.make_named_tuple('T2', [], {}, [])
+        self.assertEqual(type(factory1()).__name__, 'T1')
+
+        phlsys_namedtuple.make_named_tuple('T1', ['r1'], {}, [])
+        factory1()
+        phlsys_namedtuple.make_named_tuple('T1', [], {'d1': 1}, [])
+        self.assertFalse(hasattr(factory1(), 'd1'))
+
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
