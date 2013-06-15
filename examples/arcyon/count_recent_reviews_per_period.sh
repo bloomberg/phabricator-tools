8de55cfef72fd9b7cf42a08fe1c0bcc861acec08
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-06-15 09:54:06 +0100

    count_recent_reviews...sh: show total
    Show the total number of reviews considered when counting recent
    reviews.

--- examples/arcyon/count_recent_reviews_per_period.sh
+++ examples/arcyon/count_recent_reviews_per_period.sh
@@ -3,10 +3,11 @@ trap 'echo FAILED; exit 1' ERR
 
 arcyon="../../bin/arcyon"
 echo "last update time"
-echo "(n.b. counts last 3000 reviews or so only)"
 tempfile=`mktemp`
 $arcyon query --format-string '$humanTimeSinceDateModified' --max-results 1000 > $tempfile
 $arcyon query --format-string '$humanTimeSinceDateModified' --max-results 1000 --offset-results 1000 >> $tempfile
 $arcyon query --format-string '$humanTimeSinceDateModified' --max-results 1000 --offset-results 2000 >> $tempfile
 uniq -c $tempfile
+echo `wc -l < $tempfile` reviews considered
+echo "(n.b. counts up to last 3000 reviews or so only)"
 rm $tempfile
