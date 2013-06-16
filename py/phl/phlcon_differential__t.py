0e2b0d6772e026619d4904051dcfae749ce497e0
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-06-16 19:15:38 +0100

    phlcon_differential__t: move tests new file
    Move the phlcon_differential tests into their own file.
    
    Test Plan: $ ./run_all_tests.sh

---
+++ py/phl/phlcon_differential__t.py
@@ -0,0 +1,302 @@
+"""Test suite for phlcon_differential"""
+
+import unittest
+
+import phlcon_differential
+import phldef_conduit
+import phlsys_conduit
+
+#==============================================================================
+#                                   TEST PLAN
+#------------------------------------------------------------------------------
+# Here we detail the things we are concerned to test and specify which tests
+# cover those concerns.
+#
+# Concerns:
+# [  ] TODO
+#------------------------------------------------------------------------------
+# Tests:
+# TODO
+#==============================================================================
+
+
+class Test(unittest.TestCase):
+    def __init__(self, data):
+        super(Test, self).__init__(data)
+        self.conduit = None
+        self.reviewerConduit = None
+
+    def setUp(self):
+        test_data = phldef_conduit
+        self.conduit = phlsys_conduit.Conduit(
+            test_data.TEST_URI,
+            test_data.ALICE.user,
+            test_data.ALICE.certificate)
+
+        self.reviewerConduit = phlsys_conduit.Conduit(
+            test_data.TEST_URI,
+            test_data.BOB.user,
+            test_data.BOB.certificate)
+
+    def tearDown(self):
+        pass
+
+    def test_A_Breathing(self):
+        pass
+
+    def testNullQuery(self):
+        phlcon_differential.query(self.conduit)
+
+    def testParseCommitMessage(self):
+        title = "this is the title"
+        summary = "this is the summary"
+        test_plan = "this is the test plan"
+        reviewers = "bob"
+        message = ""
+        message += title + "\n"
+        message += "\n"
+        message += summary + "\n"
+        message += "Test Plan: " + test_plan + "\n"
+        message += "Reviewers: " + reviewers + "\n"
+        diff = phlcon_differential
+        diff.parse_response = diff.parse_commit_message(
+            self.conduit, message)
+        self.assertEqual(diff.parse_response.fields["title"], title)
+        self.assertEqual(diff.parse_response.fields["summary"], summary)
+        self.assertEqual(diff.parse_response.fields["testPlan"], test_plan)
+        # XXX: can't check reviewerPHIDs at this point
+
+    def testCreateCloseRawDiffRevision(self):
+        diff = """
+diff --git a/readme b/readme
+index d4711bb..ee5b241 100644
+--- a/readme
++++ b/readme
+@@ -7,3 +7,4 @@ and one more!!
+ -- and one last(?) one
+ alaric!
+ local stuff!
++manual conduit submission
+"""
+        message = """
+add a line to README
+
+Test Plan: I proof-read it and it looked ok
+"""
+
+        diff2 = """
+diff --git a/readme b/readme
+index d4711bb..1c634f5 100644
+--- a/readme
++++ b/readme
+@@ -7,3 +7,5 @@ and one more!!
+ -- and one last(?) one
+ alaric!
+ local stuff!
++manual conduit submission
++another line
+"""
+
+        diff_response = phlcon_differential.create_raw_diff(self.conduit, diff)
+
+        get_diff_response = phlcon_differential._get_diff(
+            self.conduit, diff_id=diff_response.id)
+        self.assertEqual(get_diff_response.id, diff_response.id)
+
+        parse_response = phlcon_differential.parse_commit_message(
+            self.conduit, message)
+        self.assertEqual(len(parse_response.errors), 0)
+
+        create_response = phlcon_differential.create_revision(
+            self.conduit, diff_response.id, parse_response.fields)
+
+        query_response_list = phlcon_differential.query(
+            self.conduit, [create_response.revisionid])
+        self.assertEqual(len(query_response_list), 1)
+        self.assertEqual(query_response_list[0].uri, create_response.uri)
+        self.assertEqual(query_response_list[0].id, create_response.revisionid)
+        self.assertEqual(
+            query_response_list[0].status,
+            phlcon_differential.REVISION_NEEDS_REVIEW)
+
+        diff2_response = phlcon_differential.create_raw_diff(
+            self.conduit, diff2)
+
+        update_response = phlcon_differential.update_revision(
+            self.conduit,
+            create_response.revisionid, diff2_response.id,
+            parse_response.fields, "updated with new diff")
+        self.assertEqual(
+            update_response.revisionid, create_response.revisionid)
+        self.assertEqual(update_response.uri, create_response.uri)
+
+        comment_response = phlcon_differential.create_comment(
+            self.reviewerConduit, create_response.revisionid, action="accept")
+        self.assertEqual(
+            comment_response.revisionid, create_response.revisionid)
+        self.assertEqual(comment_response.uri, create_response.uri)
+
+        query_response_list = phlcon_differential.query(
+            self.conduit, [create_response.revisionid])
+        self.assertEqual(len(query_response_list), 1)
+        self.assertEqual(query_response_list[0].uri, create_response.uri)
+        self.assertEqual(query_response_list[0].id, create_response.revisionid)
+        self.assertEqual(
+            query_response_list[0].status,
+            phlcon_differential.REVISION_ACCEPTED)
+
+        phlcon_differential.close(self.conduit, create_response.revisionid)
+
+        query_response_list = phlcon_differential.query(
+            self.conduit, [create_response.revisionid])
+        self.assertEqual(len(query_response_list), 1)
+        self.assertEqual(query_response_list[0].uri, create_response.uri)
+        self.assertEqual(query_response_list[0].id, create_response.revisionid)
+        self.assertEqual(
+            query_response_list[0].status, phlcon_differential.REVISION_CLOSED)
+
+    def testCreateDiffRevision(self):
+        diff = """
+diff --git a/readme b/readme
+index d4711bb..ee5b241 100644
+--- a/readme
++++ b/readme
+@@ -1,3 +1,4 @@ and one more!!
+ -- and one last(?) one
+ alaric!
+ local stuff!
++manual conduit submission
+"""
+        message = """
+add a line to README
+
+Test Plan: I proof-read it and it looked ok
+"""
+        raw_diff_response = phlcon_differential.create_raw_diff(
+            self.conduit, diff)
+        get_diff_response = phlcon_differential._get_diff(
+            self.conduit,
+            diff_id=raw_diff_response.id)
+
+        diff_response = phlcon_differential.create_diff(
+            self.conduit,
+            changes_dict=get_diff_response.changes,
+            source_machine="test_machine",
+            source_path="source_path",
+            branch="branch",
+            source_control_system="git",  # svn or git
+            source_control_path="control_path",
+            source_control_base_revision="0",
+            lint_status="none",
+            unit_status="none",
+            bookmark=None,
+            parent_revision_id=None,
+            creation_method="arcanist daemon",
+            author_phid=None,
+            arcanist_project="project",
+            repository_uuid=None)
+
+        parse_response = phlcon_differential.parse_commit_message(
+            self.conduit, message)
+        self.assertEqual(len(parse_response.errors), 0)
+
+        # rely on create_revision to raise if we get anything seriously wrong
+        phlcon_differential.create_revision(
+            self.conduit, diff_response["diffid"], parse_response.fields)
+
+    def _createRevision(self, title):
+        diff = """diff --git a/ b/"""
+        message = title + "\n\ntest plan: no test plan"
+        diff_response = phlcon_differential.create_raw_diff(self.conduit, diff)
+        parse_response = phlcon_differential.parse_commit_message(
+            self.conduit, message)
+        create_response = phlcon_differential.create_revision(
+            self.conduit, diff_response.id, parse_response.fields)
+        return create_response.revisionid
+
+    def _authorCommentAction(self, revisionid, action):
+        phlcon_differential.create_comment(
+            self.conduit, revisionid, action=action)
+
+    def _reviewCommentAction(self, revisionid, action):
+        phlcon_differential.create_comment(
+            self.reviewerConduit, revisionid, action=action)
+
+    def _getState(self, revisionid):
+        return phlcon_differential.query(self.conduit, [revisionid])[0].status
+
+    def _authorActExp(self, revisionid, action, state):
+        self._authorCommentAction(revisionid, action)
+        self.assertEqual(self._getState(revisionid), state)
+
+    def _reviewActExp(self, revisionid, action, state):
+        self._reviewCommentAction(revisionid, action)
+        self.assertEqual(self._getState(revisionid), state)
+
+    def testReviewStates(self):
+        revisionid = self._createRevision("testReviewStates")
+
+        d = phlcon_differential
+        authorActExp = self._authorActExp
+        reviewActExp = self._reviewActExp
+
+        authorActExp(revisionid, d.ACTION_RETHINK, d.REVISION_NEEDS_REVISION)
+        authorActExp(revisionid, d.ACTION_REQUEST, d.REVISION_NEEDS_REVIEW)
+        authorActExp(revisionid, d.ACTION_ABANDON, d.REVISION_ABANDONED)
+        authorActExp(revisionid, d.ACTION_RECLAIM, d.REVISION_NEEDS_REVIEW)
+
+        reviewActExp(revisionid, d.ACTION_REJECT, d.REVISION_NEEDS_REVISION)
+        reviewActExp(revisionid, d.ACTION_ACCEPT, d.REVISION_ACCEPTED)
+        reviewActExp(revisionid, d.ACTION_REJECT, d.REVISION_NEEDS_REVISION)
+        reviewActExp(revisionid, d.ACTION_ACCEPT, d.REVISION_ACCEPTED)
+
+        authorActExp(revisionid, d.ACTION_CLOSE, d.REVISION_CLOSED)
+
+    def testUpdateNoFields(self):
+        revisionid = self._createRevision("testUpdateNoFields")
+        diff = """diff --git a/ b/"""
+        diff_response = phlcon_differential.create_raw_diff(self.conduit, diff)
+        phlcon_differential.update_revision(
+            self.conduit, revisionid, diff_response.id, [], "update")
+
+    def testUpdateStrangeFields(self):
+        revisionid = self._createRevision("testUpdateStrangeFields")
+        message = "test\n\nmeh: blah\nstrange: blah\n123: blah\ntest plan: hmm"
+        diff = """diff --git a/ b/"""
+        diff_response = phlcon_differential.create_raw_diff(self.conduit, diff)
+        parse_response = phlcon_differential.parse_commit_message(
+            self.conduit, message)
+        print str(parse_response.fields)
+        phlcon_differential.update_revision(
+            self.conduit,
+            revisionid,
+            diff_response.id,
+            parse_response.fields,
+            "update")
+        # TODO: check that the strange fields appear in the summary field
+
+    def testCanGetCommitMessage(self):
+        revisionid = self._createRevision("testUpdateStrangeFields")
+        phlcon_differential.get_commit_message(self.conduit, revisionid)
+
+#------------------------------------------------------------------------------
+# Copyright (C) 2012 Bloomberg L.P.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#------------------------------- END-OF-FILE ----------------------------------
