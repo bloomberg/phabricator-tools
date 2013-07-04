0baab3876350d9fb4d5a7c3c6a6a93006ba2a4d6
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-07-04 12:53:25 +0100

    Fix aoncmd_comment: actually use conduit args
    Actually use the arguments for specifying how to connect to conduit.
    
    Test Plan:
    erase .arcconfig, comment and supply --uri

--- py/aon/aoncmd_comment.py
+++ py/aon/aoncmd_comment.py
@@ -90,7 +90,7 @@ def setupParser(parser):
 
 
 def process(args):
-    conduit = phlsys_makeconduit.make_conduit()
+    conduit = phlsys_makeconduit.make_conduit(args.uri, args.user, args.cert)
 
     if args.act_as_user:
         conduit.set_act_as_user(args.act_as_user)
