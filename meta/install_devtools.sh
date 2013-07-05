80c57e8e3ed7d69834cb85bf38b6b4a765ac1d91
Angelos Evripiotis <jonathan.evripiotis@gmail.com>
2013-07-05 22:23:09 +0100

    Script to setup tools for dev
    Make setting up for dev in a clean Ubuntu VM super-easy by supplying a
    new script to provision everything
    
    Test Plan: provisioned a new Lubuntu 13.04 VM

---
+++ meta/install_devtools.sh
@@ -0,0 +1,10 @@
+set -e  # exit on error
+sudo apt-get install pychecker
+sudo apt-get install python-pip
+sudo pip install nose
+sudo pip install flake8
+sudo pip install snakefood
+sudo pip install autopep8
+sudo pip install docformatter
+#sudo apt-get install puppet
+#(cd ../vagrant && ./puppet-apply.sh)
