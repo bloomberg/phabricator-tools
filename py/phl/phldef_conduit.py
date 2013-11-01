c4989d1ffc18fff9ffe6a337e608bd84e0461091
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-11-01 10:51:26 +0000

    phldef_conduit: add phid to canned user data
    Make it possible to test the handling of phids by copying the
    phids from the pre-recorded accounts into the canned data.
    
    Test Plan: $ ./precommit.sh

--- py/phl/phldef_conduit.py
+++ py/phl/phldef_conduit.py
@@ -27,38 +27,54 @@ import collections
 
 Account = collections.namedtuple(
     'phlcon_testdata__Account',
-    ['user', 'email', 'certificate'])
+    ['user', 'email', 'certificate', 'phid'])
 
 # if using Vagrant, there will be a Phabricator available locally
 # TODO: this should be configurable from the command-line or a config file
 TEST_URI = "http://127.0.0.1/api/"
 
-ALICE = Account(user="alice", email="alice@server.test", certificate=(
-    "35yxukrjcltwgzfmgsnj2klc2jbrnzehqz3c36ijxnicwysv3xenxymwz532pyhimpxh7jryy"
-    "nh32su2ajxahd3gp7qshyik2qwf6ntuim2acxvjnko6p2q4mhacpvugqou2wpmyqwj4hkchgc"
-    "5vh33lur723r4dexy5b3aj35v4v6ffork727ww5qk5yhhcmolbcqg3rxl6qpf53spn4aopneg"
-    "gtb675hmpx3xya3et7jrowzlkl3yw3sktvdu"))
+ALICE = Account(
+    user="alice",
+    email="alice@server.test",
+    certificate=(
+        "35yxukrjcltwgzfmgsnj2klc2jbrnzehqz3c36ijxnicwysv3xenxymwz532pyh"
+        "impxh7jryynh32su2ajxahd3gp7qshyik2qwf6ntuim2acxvjnko6p2q4mhacpv"
+        "ugqou2wpmyqwj4hkchgc5vh33lur723r4dexy5b3aj35v4v6ffork727ww5qk5y"
+        "hhcmolbcqg3rxl6qpf53spn4aopneggtb675hmpx3xya3et7jrowzlkl3yw3sktvdu"),
+    phid="PHID-USER-hncf4pdbrr53lsxn5j6z")
 
-BOB = Account(user="bob", email="bob@server.test", certificate=(
-    "6wcgqpgbqyeymlmqdhdj2xya75fa5rwttr4k7pf5klqhiiwgeqiuvsr5ulrboscwkqtumravl"
-    "33qc6in4p2hfnqal4ik3c2r33f3ezdqopl34ae4p2khvepg3bktpl5znsdrxs5jot4pk6vdz7"
-    "gasod6eksfzb2rmekawftqbaqqbfwcsaoxhc7eglq7awvmpbae6gqejpobyvglobiqw4edr7q"
-    "cqkfcyffi56hbtqzaj7m2ulqbmftdeltaqad"))
+BOB = Account(
+    user="bob",
+    email="bob@server.test",
+    certificate=(
+        "6wcgqpgbqyeymlmqdhdj2xya75fa5rwttr4k7pf5klqhiiwgeqiuvsr5ulrboscw"
+        "kqtumravl33qc6in4p2hfnqal4ik3c2r33f3ezdqopl34ae4p2khvepg3bktpl5z"
+        "nsdrxs5jot4pk6vdz7gasod6eksfzb2rmekawftqbaqqbfwcsaoxhc7eglq7awvm"
+        "pbae6gqejpobyvglobiqw4edr7qcqkfcyffi56hbtqzaj7m2ulqbmftdeltaqad"),
+    phid="PHID-USER-agn7y2uw2pj4nc5nknhe")
 
-PHAB = Account(user="phab", email="phab@server.test", certificate=(
-    "xnh5tpatpfh4pff4tpnvdv74mh74zkmsualo4l6mx7bb262zqr55vcachxgz7"
-    "ru3lrvafgzquzl3geyjxw426ujcyqdi2t4ktiv7gmrtlnc3hsy2eqsmhvgifn"
-    "2vah2uidj6u6hhhxo2j3y2w6lcsehs2le4msd5xsn4f333udwvj6aowokq5l2"
-    "llvfsl3efcucraawtvzw462q2sxmryg5y5rpicdk3lyr3uvot7fxrotwpi3ty"
-    "2b2sa2kvlpf"))
+PHAB = Account(
+    user="phab",
+    email="phab@server.test",
+    certificate=(
+        "xnh5tpatpfh4pff4tpnvdv74mh74zkmsualo4l6mx7bb262zqr55vcachxgz7"
+        "ru3lrvafgzquzl3geyjxw426ujcyqdi2t4ktiv7gmrtlnc3hsy2eqsmhvgifn"
+        "2vah2uidj6u6hhhxo2j3y2w6lcsehs2le4msd5xsn4f333udwvj6aowokq5l2"
+        "llvfsl3efcucraawtvzw462q2sxmryg5y5rpicdk3lyr3uvot7fxrotwpi3ty"
+        "2b2sa2kvlpf"),
+    phid="PHID-USER-n334wwtakshsxeau3qij")
 
 # a user that definitely won't be registered, useful for testing
-NOTAUSER = Account(user="#:)-", email="notauser@server.invalid", certificate=(
-    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-    "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-    "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
-    "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
-    "eeeeeeeeeee"))
+NOTAUSER = Account(
+    user="#:)-",
+    email="notauser@server.invalid",
+    certificate=(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+        "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+        "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd"
+        "eeeeeeeeeee"),
+    phid="PHID-NOTAUSER")
 
 #------------------------------------------------------------------------------
 # Copyright (C) 2012 Bloomberg L.P.
