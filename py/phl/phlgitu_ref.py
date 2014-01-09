9a513ab471606c2f2faebe1de0d9dd2293c530eb
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-01-09 17:48:04 +0000

    phlgitu_ref: more helpers for working with refs
    Add some more helpers for working with refs.
    
    Test Plan:
    $ ./precommit.sh

--- py/phl/phlgitu_ref.py
+++ py/phl/phlgitu_ref.py
@@ -4,15 +4,25 @@
 # -----------------------------------------------------------------------------
 # phlgitu_ref
 #
+# Public Classes:
+#   Error
+#
 # Public Functions:
 #   make_remote
 #   make_local
+#   fq_remote_to_short_local
+#   fq_to_short
+#   is_under_remote
 #
 # -----------------------------------------------------------------------------
 # (this contents block is generated, edits will be lost)
 # =============================================================================
 
 from __future__ import absolute_import
+
+
+class Error(Exception):
+    pass
 
 
 def make_remote(ref, remote):
@@ -39,6 +49,78 @@ def make_local(ref):
     """
     # TODO: check that it isn't already fully qualified
     return "refs/heads/" + ref
+
+
+def fq_remote_to_short_local(ref):
+    """Return a short Git branch name based on a fully qualified remote branch.
+
+    Raise Error if the conversion can't be done.
+
+    Usage example:
+        >>> fq_remote_to_short_local("refs/remotes/origin/mywork")
+        'mywork'
+
+        >>> fq_remote_to_short_local("refs/heads/mywork")
+        Traceback (most recent call last):
+        Error: ref can't be converted to short local: mywork
+
+    """
+    # convert to e.g. 'origin/mywork'
+    ref = fq_to_short(ref)
+
+    slash_pos = ref.find('/')
+
+    if slash_pos == -1:
+        raise Error("ref can't be converted to short local: {}".format(ref))
+
+    # disregard before and including the first slash
+    return ref[slash_pos + 1:]
+
+
+def fq_to_short(ref):
+    """Return a short Git reference based on a fully qualified name.
+
+    Raise Error if the conversion can't be done.
+
+    Usage example:
+        >>> fq_to_short("refs/heads/mywork")
+        'mywork'
+
+        >>> fq_to_short("refs/remotes/origin/mywork")
+        'origin/mywork'
+
+    """
+    refs_heads = 'refs/heads/'
+    refs_remotes = 'refs/remotes/'
+
+    if ref.startswith(refs_heads):
+        return ref[len(refs_heads):]
+
+    if ref.startswith(refs_remotes):
+        return ref[len(refs_remotes):]
+
+    raise Error("ref can't be converted to short: {}".format(ref))
+
+
+def is_under_remote(ref, remote):
+    """Return True if a Git reference is from a particular remote, else False.
+
+    Note that behavior is undefined if the ref is not fully qualified, i.e.
+    does not begin with 'refs/'.
+
+    Usage example:
+        >>> is_under_remote("refs/remotes/origin/mywork", "origin")
+        True
+
+        >>> is_under_remote("refs/remotes/origin/mywork", "alt")
+        False
+
+        >>> is_under_remote("refs/headsmywork", "origin")
+        False
+
+    """
+    return ref.startswith('refs/remotes/' + remote + '/')
+
 
 
 #------------------------------------------------------------------------------
