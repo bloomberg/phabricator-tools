f36abc610950c5a3a3e67ed4fdf74ff1e7e7320e
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-02-01 00:36:41 +0000

    phlgit_diff__t: simpler tests with phlgitu_fixture

--- py/phl/phlgit_diff__t.py
+++ py/phl/phlgit_diff__t.py
@@ -14,57 +14,42 @@
 
 from __future__ import absolute_import
 
-import os
 import unittest
 
-import phlsys_git
-import phlsys_subprocess
-
 import phlgit_diff
+import phlgitu_fixture
 
 
 class Test(unittest.TestCase):
 
-    def __init__(self, data):
-        super(Test, self).__init__(data)
-        self.path = "phlgit_diff_TestDiff"
-        self.clone = None
-
     def setUp(self):
-        # TODO: make this more portable with shutil etc.
-        phlsys_subprocess.run_commands("mkdir " + self.path)
-        phlsys_subprocess.run("git", "init", workingDir=self.path)
-        self.clone = phlsys_git.Repo(self.path)
+        pass
 
     def tearDown(self):
-        phlsys_subprocess.run_commands("rm -rf " + self.path)
-
-    def _createCommitNewFile(self, filename):
-        phlsys_subprocess.run_commands(
-            "touch " + os.path.join(self.path, filename))
-        self.clone.call("add", filename)
-        self.clone.call("commit", "-a", "-m", filename)
+        pass
 
     def testSimpleFork(self):
-        self._createCommitNewFile("README")
-        self.clone.call("branch", "fork")
-        self._createCommitNewFile("ONLY_MASTER")
-        self.clone.call("checkout", "fork")
-        self._createCommitNewFile("ONLY_FORK")
-        self._createCommitNewFile("ONLY_FORK2")
-        rawDiff = phlgit_diff.raw_diff_range_to_here(self.clone, "master")
-        rawDiff2 = phlgit_diff.raw_diff_range(self.clone, "master", "fork")
-        rawDiff3 = phlgit_diff.raw_diff_range(
-            self.clone, "master", "fork", 1000)
-        self.assertEqual(
-            set(["ONLY_FORK", "ONLY_FORK2"]),
-            phlgit_diff.parse_filenames_from_raw_diff(rawDiff))
-        self.assertEqual(
-            set(["ONLY_FORK", "ONLY_FORK2"]),
-            phlgit_diff.parse_filenames_from_raw_diff(rawDiff2))
-        self.assertEqual(
-            set(["ONLY_FORK", "ONLY_FORK2"]),
-            phlgit_diff.parse_filenames_from_raw_diff(rawDiff3))
+        with phlgitu_fixture.lone_worker_context() as worker:
+            worker.repo.call("branch", "fork")
+            worker.commit_new_file("add ONLY_MASTER", "ONLY_MASTER")
+            worker.repo.call("checkout", "fork")
+            worker.commit_new_file("add ONLY_FORK", "ONLY_FORK")
+            worker.commit_new_file("add ONLY_FORK2", "ONLY_FORK2")
+            rawDiff = phlgit_diff.raw_diff_range_to_here(
+                worker.repo, "master")
+            rawDiff2 = phlgit_diff.raw_diff_range(
+                worker.repo, "master", "fork")
+            rawDiff3 = phlgit_diff.raw_diff_range(
+                worker.repo, "master", "fork", 1000)
+            self.assertEqual(
+                set(["ONLY_FORK", "ONLY_FORK2"]),
+                phlgit_diff.parse_filenames_from_raw_diff(rawDiff))
+            self.assertEqual(
+                set(["ONLY_FORK", "ONLY_FORK2"]),
+                phlgit_diff.parse_filenames_from_raw_diff(rawDiff2))
+            self.assertEqual(
+                set(["ONLY_FORK", "ONLY_FORK2"]),
+                phlgit_diff.parse_filenames_from_raw_diff(rawDiff3))
 
 
 #------------------------------------------------------------------------------
