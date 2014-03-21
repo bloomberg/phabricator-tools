c8f54f34e116549818e51db189eabb74523da229
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-03-21 18:38:07 +0000

    phlgit_log: rename 'clone' to 'repo'
    The rest of the code base has standardised on 'repo' as the name for a
    git repository, update phlgit_log too.
    
    Test Plan:
    search for 'clone' in phlgit_log
    $ ./precommit.sh

--- py/phl/phlgit_log.py
+++ py/phl/phlgit_log.py
@@ -51,18 +51,18 @@ Revision = collections.namedtuple(
     ])
 
 
-def get_range_to_here_hashes(clone, start):
+def get_range_to_here_hashes(repo, start):
     """Return a list of strings corresponding to commits from 'start' to here.
 
     The list begins with the revision closest to but not including 'start'.
     Raise a ValueError if any of the returned values are not valid hexadecimal.
 
-    :clone: supports 'call("log")' with git log parameters
+    :repo: supports 'call("log")' with git log parameters
     :start: a reference that log will understand
     :returns: a list of strings corresponding to commits from 'start' to here.
 
     """
-    hashes = clone("log", start + "..", "--format=%H").split()
+    hashes = repo("log", start + "..", "--format=%H").split()
     if not all(c in string.hexdigits for s in hashes for c in s):
         raise ValueError(
             "phlgit_log__getRangeToHereHashes() invalid hashes\n"
@@ -71,45 +71,45 @@ def get_range_to_here_hashes(clone, start):
     return hashes
 
 
-def get_last_n_commit_hashes(clone, n):
+def get_last_n_commit_hashes(repo, n):
     """Return a list of strings corresponding to the last commits.
 
     The list begins with the oldest revision.
     Raise a ValueError if any of the returned values are not valid hexadecimal.
     Raise an Exception if less values than expected are returned.
 
-    :clone: supports 'call("log")' with git log parameters
+    :repo: supports 'call("log")' with git log parameters
     :returns: a string corresponding to the last commit ('HEAD')
 
     """
-    return get_last_n_commit_hashes_from_ref(clone, n, 'HEAD')
+    return get_last_n_commit_hashes_from_ref(repo, n, 'HEAD')
 
 
-def get_last_commit_hash(clone):
+def get_last_commit_hash(repo):
     """Return a string corresponding to the last commit ('HEAD').
 
     Raise a ValueError if the returned value is not valid hexadecimal.
 
-    :clone: supports 'call("log")' with git log parameters
+    :repo: supports 'call("log")' with git log parameters
     :returns: a string corresponding to the last commit ('HEAD')
 
     """
-    return get_last_commit_hash_from_ref(clone, 'HEAD')
+    return get_last_commit_hash_from_ref(repo, 'HEAD')
 
 
-def get_last_n_commit_hashes_from_ref(clone, n, ref):
+def get_last_n_commit_hashes_from_ref(repo, n, ref):
     """Return a list of strings corresponding to the last 'n' commits at 'ref'.
 
     The list begins with the oldest revision.
     Raise a ValueError if any of the returned values are not valid hexadecimal.
     Raise an Exception if less values than expected are returned.
 
-    :clone: supports 'call("log")' with git log parameters
+    :repo: supports 'call("log")' with git log parameters
     :returns: a string corresponding to the last commit ('HEAD')
 
     """
     assert n >= 0
-    hashes = clone("log", ref, "-n", str(n), "--format=%H").split()
+    hashes = repo("log", ref, "-n", str(n), "--format=%H").split()
     if len(hashes) < n:
         raise ValueError(
             "less hashes than expected\n" + str(hashes))
@@ -121,32 +121,32 @@ def get_last_n_commit_hashes_from_ref(clone, n, ref):
     return hashes
 
 
-def get_last_commit_hash_from_ref(clone, ref):
+def get_last_commit_hash_from_ref(repo, ref):
     """Return a string corresponding to the commit referred to by 'ref'.
 
-    :clone: supports 'call("log")' with git log parameters
+    :repo: supports 'call("log")' with git log parameters
     :ref: a reference that log will understand
     :returns: a string corresponding to the commit referred to by 'ref'
 
     """
-    return get_last_n_commit_hashes_from_ref(clone, 1, ref)[0]
+    return get_last_n_commit_hashes_from_ref(repo, 1, ref)[0]
 
 
-def get_range_hashes(clone, start, end):
+def get_range_hashes(repo, start, end):
     """Return a list of strings corresponding to commits from 'start' to 'end'.
 
     The list begins with the revision closest to but not including 'start'.
     Raise a ValueError if any of the returned values are not valid hexadecimal.
 
-    :clone: supports 'call("log")' with git log parameters
+    :repo: supports 'call("log")' with git log parameters
     :start: a reference that log will understand
     :end: a reference that log will understand
     :returns: a list of strings corresponding to commits from 'start' to 'end'.
 
     """
-    assert clone("rev-parse", "--revs-only", start)
-    assert clone("rev-parse", "--revs-only", end)
-    hashes = clone("log", start + ".." + end, "--format=%H").split()
+    assert repo("rev-parse", "--revs-only", start)
+    assert repo("rev-parse", "--revs-only", end)
+    hashes = repo("log", start + ".." + end, "--format=%H").split()
     if not all(c in string.hexdigits for s in hashes for c in s):
         raise ValueError(
             "phlgit_log__getRangeHashes() invalid hashes\n" + str(hashes))
@@ -175,37 +175,37 @@ def make_revision_from_full_message(message):
         message='\n'.join(lines[7:]))
 
 
-def make_revision_from_hash(clone, commitHash):
-    """Return a 'phlgit_log__Revision' based on 'commitHash' from the clone.
-    Raise an exception if the clone does not return a valid FullMessage from
-    the commitHash.
+def make_revision_from_hash(repo, commitHash):
+    """Return a 'phlgit_log__Revision' based on 'commitHash' from the repo.
+    Raise an exception if the repo does not return a valid FullMessage from the
+    commitHash.
 
-    :clone: something that supports "call()" with git commands
+    :repo: something that supports "call()" with git commands
     :commitHash: a string containing the hash to get the message of
     :returns: a 'phlgit_log__Revision' based on the 'commitHash'
 
     """
     fmt = "%H%n%h%n%ae%n%an%n%ce%n%cn%n%s%n%b"
-    fullMessage = clone("log", commitHash + "^!", "--format=" + fmt)
+    fullMessage = repo("log", commitHash + "^!", "--format=" + fmt)
     revision = make_revision_from_full_message(fullMessage)
     return revision
 
 
-def make_revisions_from_hashes(clone, hashes):
+def make_revisions_from_hashes(repo, hashes):
     """Return a list of 'phlgit_log__Revision' from 'hashes'.
 
-    Raise an exception if the clone does not return a valid FullMessage
+    Raise an exception if the repo does not return a valid FullMessage
     from any of 'hashes'.
 
-    :clone: something that supports "call()" with git commands
+    :repo: something that supports "call()" with git commands
     :returns: a list of 'phlgit_log__Revision'
 
     """
-    revisions = [make_revision_from_hash(clone, h) for h in hashes]
+    revisions = [make_revision_from_hash(repo, h) for h in hashes]
     return revisions
 
 
-def get_author_names_emails_from_hashes(clone, hashes):
+def get_author_names_emails_from_hashes(repo, hashes):
     """Return list of (name, email) of the committers in 'hashes'.
 
     Authors will only appear in the list once, at their earliest appearance.
@@ -213,15 +213,15 @@ def get_author_names_emails_from_hashes(clone, hashes):
     someone appearing multiple times with different names but the same email
     will only appear once in the list.
 
-    Raise an exception if the clone does not return a valid FullMessage from
+    Raise an exception if the repo does not return a valid FullMessage from
     the commitHash.
 
-    :clone: something that supports "call()" with git commands
+    :repo: something that supports "call()" with git commands
     :hashes: a list of strings containing the hashes to get the messages of
     :returns: a list of unique committer emails in commit order from 'start..'
 
     """
-    revisions = make_revisions_from_hashes(clone, hashes)
+    revisions = make_revisions_from_hashes(repo, hashes)
     observedEmails = set()
     uniqueAuthors = []
     for r in revisions:
@@ -233,10 +233,10 @@ def get_author_names_emails_from_hashes(clone, hashes):
     return uniqueAuthors
 
 
-def get_range_to_here_raw_body(clone, start):
+def get_range_to_here_raw_body(repo, start):
     # TODO: we actually want something that can return an list of bodies
     # TODO: '-n ' '1' is a hack until we return a list
-    return clone("log", start + "..", "--format=format:%B", "-n", "1")
+    return repo("log", start + "..", "--format=format:%B", "-n", "1")
 
 
 #------------------------------------------------------------------------------
