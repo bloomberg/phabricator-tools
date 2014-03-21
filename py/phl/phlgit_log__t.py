0811a9bf2c34693cd0caef94636e990a80072fbf
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-03-21 18:42:20 +0000

    phlgit_log__t: rename 'clone' to 'repo'
    The rest of the code base has standardised on 'repo' as the name for a
    git repository, update phlgit_log__t too.
    
    Test Plan:
    search for 'clone' in phlgit_log__t
    $ ./precommit.sh

--- py/phl/phlgit_log__t.py
+++ py/phl/phlgit_log__t.py
@@ -28,7 +28,7 @@ class Test(unittest.TestCase):
     def __init__(self, data):
         super(Test, self).__init__(data)
         self.path = "phlgit_diff_TestDiff"
-        self.clone = None
+        self.repo = None
         self.authorName = "No one"
         self.authorEmail = "noone@nowhere.com"
         self.author = self.authorName + " <" + self.authorEmail + ">"
@@ -37,7 +37,7 @@ class Test(unittest.TestCase):
         # TODO: make this more portable with shutil etc.
         phlsys_subprocess.run_commands("mkdir " + self.path)
         phlsys_subprocess.run("git", "init", workingDir=self.path)
-        self.clone = phlsys_git.Repo(self.path)
+        self.repo = phlsys_git.Repo(self.path)
 
     def tearDown(self):
         phlsys_subprocess.run_commands("rm -rf " + self.path)
@@ -45,86 +45,86 @@ class Test(unittest.TestCase):
     def _createCommitNewFile(self, filename, subject=None, message=None):
         phlsys_subprocess.run_commands(
             "touch " + os.path.join(self.path, filename))
-        self.clone("add", filename)
+        self.repo("add", filename)
         if not subject:
             if message:
                 raise Exception("didn't expect message with empty subject")
-            self.clone(
+            self.repo(
                 "commit", "-a", "-m", filename,
                 "--author", self.author)
         elif not message:
-            self.clone(
+            self.repo(
                 "commit", "-a", "-m", subject,
                 "--author", self.author)
         else:
             message = subject + "\n\n" + message
-            self.clone(
+            self.repo(
                 "commit", "-a", "-m", message,
                 "--author", self.author)
 
 #     def testNoCommits(self):
-#         hashes = get_range_to_here_hashes(self.clone, "HEAD")
+#         hashes = get_range_to_here_hashes(self.repo, "HEAD")
 #         self.assertIsNotNone(hashes)
 #         self.assertTrue(not hashes)
 #         self.assertIsInstance(hashes, list)
-#         head = get_last_commit_hash(self.clone)
+#         head = get_last_commit_hash(self.repo)
 #         self.assertIsNone(head)
-#         head2 = get_last_n_commit_hashes(self.clone, 1)
+#         head2 = get_last_n_commit_hashes(self.repo, 1)
 #         self.assertIsNotNone(head2)
 #         self.assertEqual(head, head2[0])
 
     def testOneCommit(self):
         self._createCommitNewFile("README")
-        hashes = phlgit_log.get_range_to_here_hashes(self.clone, "HEAD")
+        hashes = phlgit_log.get_range_to_here_hashes(self.repo, "HEAD")
         self.assertIsNotNone(hashes)
         self.assertTrue(not hashes)
         self.assertIsInstance(hashes, list)
-        head = phlgit_log.get_last_commit_hash(self.clone)
+        head = phlgit_log.get_last_commit_hash(self.repo)
         self.assertIsNotNone(head)
-        head2 = phlgit_log.get_last_n_commit_hashes(self.clone, 1)
+        head2 = phlgit_log.get_last_n_commit_hashes(self.repo, 1)
         self.assertIsNotNone(head2)
         self.assertEqual(head, head2[0])
         self.assertListEqual(
-            phlgit_log.get_last_n_commit_hashes(self.clone, 0), [])
+            phlgit_log.get_last_n_commit_hashes(self.repo, 0), [])
         self.assertRaises(
-            ValueError, phlgit_log.get_last_n_commit_hashes, self.clone, 2)
+            ValueError, phlgit_log.get_last_n_commit_hashes, self.repo, 2)
 
     def testTwoCommits(self):
         self._createCommitNewFile("README")
         self._createCommitNewFile("README2")
-        head = phlgit_log.get_last_commit_hash(self.clone)
+        head = phlgit_log.get_last_commit_hash(self.repo)
         self.assertIsNotNone(head)
-        hashes = phlgit_log.get_last_n_commit_hashes(self.clone, 2)
+        hashes = phlgit_log.get_last_n_commit_hashes(self.repo, 2)
         self.assertIsNotNone(hashes)
         self.assertEqual(head, hashes[-1])
         self.assertListEqual(hashes, hashes)
 
     def testSimpleFork(self):
         self._createCommitNewFile("README")
-        self.clone("branch", "fork")
+        self.repo("branch", "fork")
         self._createCommitNewFile("ONLY_MASTER")
-        self.clone("checkout", "fork")
+        self.repo("checkout", "fork")
         self._createCommitNewFile("ONLY_FORK", "ONLY_FORK", "BODY\nBODY")
         self._createCommitNewFile("ONLY_FORK2")
 
-        log = phlgit_log.get_range_to_here_raw_body(self.clone, "master")
+        log = phlgit_log.get_range_to_here_raw_body(self.repo, "master")
         self.assertIn("ONLY_FORK", log)
         self.assertNotIn("ONLY_MASTER", log)
         self.assertNotIn("README", log)
 
-        hashes = phlgit_log.get_range_to_here_hashes(self.clone, "master")
-        hashes2 = phlgit_log.get_range_hashes(self.clone, "master", "fork")
+        hashes = phlgit_log.get_range_to_here_hashes(self.repo, "master")
+        hashes2 = phlgit_log.get_range_hashes(self.repo, "master", "fork")
         self.assertListEqual(hashes, hashes2)
-        r0 = phlgit_log.make_revision_from_hash(self.clone, hashes[0])
+        r0 = phlgit_log.make_revision_from_hash(self.repo, hashes[0])
         self.assertEqual(r0.subject, "ONLY_FORK")
         self.assertEqual(r0.message, "BODY\nBODY\n")
-        r1 = phlgit_log.make_revision_from_hash(self.clone, hashes[1])
+        r1 = phlgit_log.make_revision_from_hash(self.repo, hashes[1])
         self.assertEqual(r1.subject, "ONLY_FORK2")
         self.assertIsNotNone(r1.message)
         self.assertIsInstance(r1.message, str)
 
         committers = phlgit_log.get_author_names_emails_from_hashes(
-            self.clone, hashes)
+            self.repo, hashes)
         self.assertEqual(len(committers), 1)
         self.assertEqual(committers[0], (self.authorName, self.authorEmail))
 
