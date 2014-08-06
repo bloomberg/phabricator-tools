edc199189a6259f78b8c6b0d5554b9988f9b4352
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-08-06 15:28:05 +0100

    Allow urls with 'arcyd rm-repo' for ease of use
    Make it easier to remove repo configs by adding a flag to lookup urls.
    
    Test Plan
    $ ./precommit.sh
    $ ./testbed/arcyd/test_shell.sh
        $ cd ../arcyd
        $ arcyd stop
        $ arcyd rm-repo --lookup-url ../origin
        $ git show
        .. looks right ..

--- py/abd/abdcmd_rmrepo.py
+++ py/abd/abdcmd_rmrepo.py
@@ -20,6 +20,7 @@ import shutil
 
 import phlsys_pid
 
+import abdi_repoargs
 import abdt_fs
 
 
@@ -33,6 +34,12 @@ def setupParser(parser):
         type=str,
         metavar='STR',
         help="string identifier of the repository to remove.")
+
+    parser.add_argument(
+        '--lookup-url',
+        action='store_true',
+        help="treat 'name' as a full url and lookup the associated repo"
+             "config. Fail if there is more than one repo with the same url.")
 
 
 def _remove_file_ignore_fail(path):
@@ -63,11 +70,47 @@ def process(args):
             raise Exception("cannot remove repo whilst arcyd is running.")
 
         repo_name = args.name
+        if args.lookup_url:
+            repo_name = _determine_name_from_url(fs, repo_name)
 
         _remove_file_ignore_fail(fs.layout.repo_try(repo_name))
         _remove_file_ignore_fail(fs.layout.repo_ok(repo_name))
         _remove_dir_ignore_fail(fs.layout.repo(repo_name))
         fs.remove_repo_config(repo_name)
+
+
+def _determine_name_from_url(fs, repo_url):
+    """Return the string name of the repository for 'repo_url' or raise.
+
+    If there is not exactly one repository config that refers to 'repo_url'
+    then raise.
+
+    :fs: abdt_fs.Accessor to use to determine the repo name from the url
+    :repo_url: string clone url of the repository we fetch from / push to
+    :returns: string name of the matching config
+
+    """
+    repo_config_path_list = fs.repo_config_path_list()
+    repo_name_config_list = abdi_repoargs.parse_config_file_list(
+        repo_config_path_list)
+
+    candidate_names = []
+
+    for repo_name, repo_config in repo_name_config_list:
+        url = abdi_repoargs.get_repo_url(repo_config)
+        if repo_url == url:
+            candidate_names.append(repo_name)
+
+    if not candidate_names:
+        raise Exception("url '{url}' didn't match any names".format(
+            url=repo_url))
+
+    if len(candidate_names) > 1:
+        raise Exception("url '{url}' matches many names:\n{names}\n".format(
+            url=repo_url,
+            names=candidate_names))
+
+    return candidate_names[0]
 
 
 # -----------------------------------------------------------------------------
