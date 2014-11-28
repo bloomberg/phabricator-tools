72e0c41e7b20130bb3bca53302e49d97c8c43c85
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-11-28 16:28:46 +0000

    Arcyd: make io_log access thread-safe
    Trivially add a threading.Lock around access to
    abt_logging.on_io_event() internals, in preparation for enabling
    multi-threaded arcyd.
    
    Test Plan:
    $ ./precommit.sh

--- py/abd/abdt_logging.py
+++ py/abd/abdt_logging.py
@@ -21,12 +21,14 @@ from __future__ import absolute_import
 import datetime
 import logging
 import os
+import threading
 
 import phlsys_subprocess
 
 
 _LOGGER = logging.getLogger(__name__)
 _EXTERNAL_SYSTEM_ERROR_LOGGER = None
+_IO_LOG_LOCK = threading.Lock()
 _IO_LOG_PATH = None
 
 
@@ -44,7 +46,8 @@ def clear_external_system_error_logger():
 def set_io_log_path(path):
     assert path
     global _IO_LOG_PATH
-    _IO_LOG_PATH = os.path.abspath(path)
+    with _IO_LOG_LOCK:
+        _IO_LOG_PATH = os.path.abspath(path)
 
 
 def on_system_error(identifier, detail):
@@ -89,11 +92,12 @@ def on_retry_exception(identifier, detail, e, delay):
 
 
 def on_io_event(identifier, detail):
-    if _IO_LOG_PATH:
-        with open(_IO_LOG_PATH, 'a') as f:
-            now = str(datetime.datetime.utcnow())
-            description = '{}: {} - {}\n'.format(now, identifier, detail)
-            f.write(description)
+    with _IO_LOG_LOCK:
+        if _IO_LOG_PATH:
+            with open(_IO_LOG_PATH, 'a') as f:
+                now = str(datetime.datetime.utcnow())
+                description = '{}: {} - {}\n'.format(now, identifier, detail)
+                f.write(description)
 
 
 # -----------------------------------------------------------------------------
