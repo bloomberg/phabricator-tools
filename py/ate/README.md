50b04dd2932b15d3e0481ce85a4d3bbbde791845
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-12-03 15:15:41 +0000

    Arcyd-tester: add bare bones for new binary
    Add a new binary to phabricator-tools intended to exercise arcyd.
    
    Test Plan:
    $ ./precommit.sh

---
+++ py/ate/README.md
@@ -0,0 +1,6 @@
+# ate
+* `atecmd_arcydtester.py` -
+Arcyd tester - command-line utility to exercise arcyd.
+
+-----
+*please note: this file is generated, edits will be lost*
---
+++ py/ate/atecmd_arcydtester.py
@@ -0,0 +1,45 @@
+"""Arcyd tester - command-line utility to exercise arcyd."""
+# =============================================================================
+# CONTENTS
+# -----------------------------------------------------------------------------
+# atecmd_arcydtester
+#
+# Public Functions:
+#   main
+#
+# -----------------------------------------------------------------------------
+# (this contents block is generated, edits will be lost)
+# =============================================================================
+
+from __future__ import absolute_import
+
+import argparse
+
+_USAGE_EXAMPLES = """
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=__doc__,
+        epilog=_USAGE_EXAMPLES)
+
+    parser.parse_args()
+
+
+# -----------------------------------------------------------------------------
+# Copyright (C) 2014 Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------ END-OF-FILE ----------------------------------
