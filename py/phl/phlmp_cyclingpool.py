d54e0fca330f5637f760d007fea404481e926681
Angelos Evripiotis <jevripiotis@bloomberg.net>
2015-01-16 17:48:10 +0000

    fixup! phlmp_cyclingpool:MultiprocessingWorkerFinishError
    Correct copyright notice
    
    Test Plan:
    $ ./precommit.sh

--- py/phl/phlmp_cyclingpool.py
+++ py/phl/phlmp_cyclingpool.py
@@ -322,7 +322,7 @@ def _worker_process(job_list, work_queue, results_queue):
 
 
 # -----------------------------------------------------------------------------
-# Copyright (C) 2014 Bloomberg Finance L.P.
+# Copyright (C) 2014-2015 Bloomberg Finance L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
