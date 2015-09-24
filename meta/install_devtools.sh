b2866490fe14c0e95e4eb3df08f43bd48985f687
Chandan Singh <csingh43@bloomberg.net>
2015-09-24 13:39:29 +0100

    Meta: Use pip to install pylint
    pylint is available for install using pip which is the preferred method
    of install for python packages over apt-get. Using apt-get to install
    pylint often results in different versions of pylint being installed on
    different systems. Using pip to install pylint will ensure that same
    version is being used on all systems.
    
    Test plan:
    
    (Manual) Check the version number of pylint installed using
    "pylint --version". Now, run /meta/install_devtools.sh. This should
    update pylint to the latest version. Check version number again and
    ensure that the latest version is installed.

--- meta/install_devtools.sh
+++ meta/install_devtools.sh
@@ -1,7 +1,7 @@
 set -e  # exit on error
-sudo apt-get install pylint
 sudo apt-get install pychecker
 sudo apt-get install python-pip
+sudo pip install pylint
 sudo pip install nose
 sudo pip install flake8
 sudo pip install snakefood
