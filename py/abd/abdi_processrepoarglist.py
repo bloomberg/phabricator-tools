19361bc9d1cca14f531da3a2131b153f854346cc
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-09-30 09:04:42 +0100

    Arcyd: Fix cache race condition when overrunning
    Arcyd worker processes which overran could incorrectly consume the
    'newness' of a repository which had been updated while it was
    overrunning. This is because it would happen unconditionally, whether or
    not the repository snoop url now pointed to different content.
    
    Fix this by using function 'watcher.merge_data_consume_only()' which
    merges the data for URLs and also consumes the 'newness' of the URL only
    if the content hashes match.
    
    This commit also fixes the fail in Arcyd tester which was introduced in
    a previous commit.
    
    Test plan:
    $ ./precommit.sh

--- py/abd/abdi_processrepoarglist.py
+++ py/abd/abdi_processrepoarglist.py
@@ -207,9 +207,11 @@ class _RecordingWatcherWrapper(object):
         self._tested_urls.add(url)
         return self._watcher.has_url_recently_changed(url)
 
-    @property
-    def tested_urls(self):
-        return self._tested_urls
+    def get_data_for_merging(self):
+        data = self._watcher.get_data_for_merging()
+        tested = self._tested_urls
+        new_data = {k: v for k, v in data.iteritems() if k in tested}
+        return new_data
 
 
 class _RepoActiveRetryState(object):
@@ -321,7 +323,7 @@ class _ArcydManagedRepository(object):
         return (
             self._review_cache.active_reviews - old_active_reviews,
             self._active_state,
-            watcher.tested_urls,
+            watcher.get_data_for_merging(),
             self._refcache_repo.peek_hash_ref_pairs(),
             self._differ_cache.get_cache()
         )
@@ -331,7 +333,7 @@ class _ArcydManagedRepository(object):
         (
             active_reviews,
             active_state,
-            tested_urls,
+            watcher_data,
             hash_ref_pairs,
             differ_cache
         ) = results
@@ -342,8 +344,7 @@ class _ArcydManagedRepository(object):
         self._differ_cache.set_cache(differ_cache)
 
         # merge in the consumed urls from the worker
-        for url in tested_urls:
-            self._url_watcher_wrapper.watcher.has_url_recently_changed(url)
+        self._url_watcher_wrapper.watcher.merge_data_consume_only(watcher_data)
 
 
 class _ConduitManager(object):
